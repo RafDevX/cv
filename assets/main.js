1371bb36b7d535da9a31397e22128fdda5676ffe
Rafael Oliveira <rafdev.x@gmail.com>
2024-05-10 17:18:12 +0200

    open external links in a new tab

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,22 @@ import * as params from "@params";
 let originalTitle = document.title;
 
 window.addEventListener("beforeprint", () => {
-	originalTitle = document.title;
-	document.title = params.printTitle;
+    originalTitle = document.title;
+    document.title = params.printTitle;
 });
 
 window.addEventListener("afterprint", () => {
-	document.title = originalTitle;
+    document.title = originalTitle;
 });
+
+for (const el of document.getElementsByTagName("a")) {
+    const href = el.getAttribute("href");
+    if (!href?.startsWith("http")) continue;
+
+    const url = new URL(href);
+
+    if (url.origin !== window.location.origin) {
+        el.setAttribute("target", "_blank");
+        el.setAttribute("rel", "noreferrer");
+    }
+}
